2f2c64848c6f9af57b3fb618aefefcf0aeb7daac
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-07-05 16:36:27 -0600

    feat(Footer): Finished footer design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,30 @@
-import { IconLemon, IconBrandGithub } from "@tabler-icons";
+import { IconBrandGithub, IconLemon } from "@tabler-icons";
 
 export default function Footer() {
   return (
-    <footer class="border-t-2 border-[#1F1F1F] p-2">
-      <a href="#" class="inline-flex gap-1 border-2 border-black rounded-md px-2 py-1">
-        <IconLemon size={24} stroke="1.5" color="black" />
-        <span class="font-semibold">
-          Made with fresh
-        </span>
-      </a>
-      <a href="#" class="inline-flex gap-1">
-        <IconBrandGithub size={24} stroke="1.5" color="black" />
-        <span>
-          Source code
-        </span>
-      </a>
+    <footer class="border-t-2 border-[#1F1F1F] p-2 flex justify-center items-center">
+      <div class="flex gap-4">
+        <a
+          href="#"
+          class="inline-flex gap-1 border-2 border-black rounded-md px-2 py-1 bg-yellow-300"
+          style="box-shadow: black 2px 2px"
+        >
+          <IconLemon size={24} stroke="1.5" color="black" />
+          <span class="font-semibold">
+            Made with Fresh
+          </span>
+        </a>
+        <a
+          href="#"
+          class="inline-flex gap-1 border-2 border-black rounded-md px-2 py-1"
+          style="box-shadow: black 2px 2px"
+        >
+          <IconBrandGithub size={24} stroke="1.75" color="black" />
+          <span class="font-semibold">
+            Source code
+          </span>
+        </a>
+      </div>
     </footer>
   );
 }
